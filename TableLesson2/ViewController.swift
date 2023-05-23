f3df216d4013032f49cfe35fea76c3b7b0a6548d
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-05-23 11:19:27 +0200

    Changed the array from cars to namesList and populated it with names

--- TableLesson2/ViewController.swift
+++ TableLesson2/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var cars = ["Bmw", "Toyota", "Ford", "Audi", "Isuzu", "McLaren"]
+    var namesList = ["Sizwe", "Victor", "Sibalukhulu", "Rolva", "Monono", "Karabo", "Ntokozo",
+                     "Melissa", "Muzi", "Lesego", "Mandla", "Jessica", "Zizwe", "Musawenkosi", "Sanele", "Asanele","Busi", "Thandeka", "Mahlangu", "Ndevu"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +31,7 @@ extension ViewController : UITableViewDataSource{
       
         //First we were returning 10 then changed to return the number of items in the array
         
-        return cars.count
+        return namesList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -40,7 +41,7 @@ extension ViewController : UITableViewDataSource{
         //first were returning the the text below then changed to return the items in the array
         //cell.textLabel?.text = "I love programming"
         
-        cell.textLabel?.text = cars[indexPath.row]
+        cell.textLabel?.text = namesList[indexPath.row]
         return cell
     }
     
